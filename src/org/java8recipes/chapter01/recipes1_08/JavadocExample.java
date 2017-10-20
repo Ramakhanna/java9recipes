ab55307d8fc81052c43a8e2e1dc87f0da40445a0
Ramakhanna <ramakhannaece093@gmail.com>
2017-10-20 09:29:24 +0530

    Commit from Eclipse Oxygen Recipes J8

---
+++ src/org/java8recipes/chapter01/recipes1_08/JavadocExample.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.java8recipes.chapter01.recipes1_08;
+
+/**
+ *
+ * @author ramakhanna
+ */
+
+/**
+ * Accepts unlimited number of values and
+ * return the sum
+ * @param sums must be array of bigintegers
+ * @return returns sums of all array elements
+ */
+public class JavadocExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+     System.out.println("test");   
+    }
+    
+}
